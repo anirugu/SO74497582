5ffe918cf207a89d54f9c1615b7f97cb02b657bd
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-11-19 10:27:33 +0530

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace SO74497582.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+
+        [HttpPost]
+        [Route("test")]
+        public async Task<IActionResult> Test()
+        {
+            using (StreamReader reader
+                 = new StreamReader(Request.Body, Encoding.UTF8, true, 1024, true))
+            {
+                string content = await reader.ReadToEndAsync();
+                return View();
+            }
+            ///Service1Client o = new Service1Client();
+            //o.Test(myXML);
+
+
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SO74497582.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
